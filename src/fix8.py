7985aa50a71ba7593199bb4c8b29ec6bc95f3863
Naser Al Madi <nsalmadi@colby.edu>
2023-07-23 22:06:23 -0400

    Fix8 small executables and v1, removed EMTK file and sklearn, still needs refactoring

--- src/fix8.py
+++ src/fix8.py
@@ -5,6 +5,11 @@
 #          Najam Tariq
 #
 # URL: <https://github.com/nalmadi/fix8>
+#
+#
+# The algorithms were implemented by:
+#          Jon Carr
+# URL: <https://github.com/jwcarr/drift>
 # For license information, see LICENSE.TXT
 
 """
@@ -16,10 +21,11 @@ correction methods for eye tracking data in reading tasks.
 
 """
 
-from PyQt5.QtWidgets import *
+# from PyQt5.QtWidgets import *
 import time
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QFileDialog,
+# from PyQt5.QtCore import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QFrame, QApplication, QCheckBox, QComboBox, QFileDialog,
                              QHBoxLayout, QLabel, QSlider, QMainWindow, QMessageBox,
                              QPushButton, QSizePolicy, QVBoxLayout, QWidget, QButtonGroup, QLineEdit, QListWidget, QListWidgetItem, QSpinBox)
 import matplotlib.image as mpimg
@@ -27,7 +33,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolBar
-import emip_toolkit as emtk
 from matplotlib.patches import Rectangle
 import json
 from os import listdir
@@ -38,6 +43,9 @@ import copy
 #import matplotlib.animation as animation
 from datetime import date
 from pathlib import Path
+
+from PIL import Image
+import pandas as pd
 
 # from PySide2 import QtWidgets
 # from PyQt5 import QtWidgets
@@ -117,7 +125,7 @@ class Fix8(QMainWindow):
         # fields relating to color filters
         self.fixation_color = 'red'
         self.saccade_color = 'blue' 
-               
+        
         # fields related to duration filters
         self.lesser_value = 0
         self.greater_value = 0
@@ -175,16 +183,16 @@ class Fix8(QMainWindow):
                     updated_correction = da.attach(copy.deepcopy(fixation_XY), line_Y)[0]
                 elif self.algorithm == 'chain':
                     updated_correction = da.chain(copy.deepcopy(fixation_XY), line_Y)[0]
-                elif self.algorithm == 'cluster':
-                    updated_correction = da.cluster(copy.deepcopy(fixation_XY), line_Y)[0]
+                # elif self.algorithm == 'cluster':
+                #     updated_correction = da.cluster(copy.deepcopy(fixation_XY), line_Y)[0]
                 elif self.algorithm == 'merge':
                     self.suupdated_correction = da.merge(copy.deepcopy(fixation_XY), line_Y)[0]
                 elif self.algorithm == 'regress':
                     updated_correction = da.regress(copy.deepcopy(fixation_XY), line_Y)[0]
                 elif self.algorithm == 'segment':
                     updated_correction = da.segment(copy.deepcopy(fixation_XY), line_Y)[0]
-                elif self.algorithm == 'split':
-                    updated_correction = da.split(copy.deepcopy(fixation_XY), line_Y)[0]
+                # elif self.algorithm == 'split':
+                #     updated_correction = da.split(copy.deepcopy(fixation_XY), line_Y)[0]
                 elif self.algorithm == 'stretch':
                     updated_correction = da.stretch(copy.deepcopy(fixation_XY), line_Y)[0]
                 elif self.algorithm == 'warp':
@@ -348,7 +356,7 @@ class Fix8(QMainWindow):
     '''when a trial from the trial list is double clicked, find the fixations of the trial
         parameters:
         item - the value passed through when clicking a trial object in the list'''
-    def trial_double_clicked(self,item):
+    def trial_double_clicked(self, item):
         # reset times saved if a DIFFERENT trial was selected
         self.trial_name = item.text()
         if self.trials[item.text()] != self.trial_path:
@@ -377,10 +385,199 @@ class Fix8(QMainWindow):
         self.draw_canvas(self.corrected_fixations,draw_all=True)
         self.progress_bar_updated(self.current_fixation, draw=False)
 
+######################## from EMTK #################################
+    
+
+    def find_background_color(self, img):
+        """Private function that identifies the background color of the image
+        Parameters
+        ----------
+        img : PIL.Image
+            a PIL (pillow fork) Image object
+        Returns
+        -------
+        str
+            the color of the background of the image
+        """
+
+        img = img.convert('L')  # Convert to grayscale
+        threshold = 80
+        img = img.point(lambda x: 0 if x < threshold else 255, '1')  # Apply threshold and convert to black and white
+
+        width, height = img.size
+
+        color_result = []
+        box_size = min(width, height) // 20
+
+        # Move a tiny rectangle box to obtain most common color
+        for x, y in zip(range(0, width, box_size), range(0, height, box_size)):
+            box = (x, y, x + box_size, y + box_size)
+            minimum, maximum = img.crop(box).getextrema()
+            color_result.append(minimum)
+            color_result.append(maximum)
+
+        # Analyze and determine the background color
+        if color_result.count(255) > color_result.count(0):
+            bg_color = 'white'
+        else:
+            bg_color = 'black'
+
+        return bg_color
+
+
+    def EMTK_find_aoi(self, image=None, image_path=None, img=None, level="sub-line", margin_height=4, margin_width=7):
+        """Find Area of Interest in the given image and store the aoi attributes in a Pandas Dataframe
+        Parameters
+        ----------
+        image : str
+            filename for the image, e.g. "vehicle_java.jpg"
+        image_path : str
+            path for all images, e.g. "emip_dataset/stimuli/"
+        img : PIL.Image, optional
+            PIL.Image object if user chooses to input an PIL image object
+        level : str, optional
+            level of detection in AOIs, "line" for each line as an AOI or "sub-line" for each token as an AOI
+        margin_height : int, optional
+            marginal height when finding AOIs, use smaller number for tight text layout
+        margin_width : int, optional
+            marginal width when finding AOIs, use smaller number for tight text layout
+        Returns
+        -------
+        pandas.DataFrame
+            a pandas DataFrame of area of interest detected by the method
+        """
+
+        if img is None:
+            if image is None or image_path is None:
+                return
+            # img = Image.open(image_path + image).convert('1')
+            img = Image.open(image_path + image)
+            img = img.convert('L')  # Convert to grayscale
+            threshold = 80
+            img = img.point(lambda x: 0 if x < threshold else 255, '1')  # Apply threshold and convert to black and white
+
+        else:
+            img = img.convert('L')  # Convert to grayscale
+            threshold = 80
+            img = img.point(lambda x: 0 if x < threshold else 255, '1')  # Apply threshold and convert to black and white
+
+        width, height = img.size
+
+        # Detect the background color
+        bg_color = self.find_background_color(img)
+        # print("bg_color: ", bg_color)
+
+        left, right = 0, width
+
+        vertical_result, upper_bounds, lower_bounds = [], [], []
+
+        # Move the detecting rectangle from the top to the bottom of the image
+        for upper in range(height - margin_height):
+
+            lower = upper + margin_height
+
+            box = (left, upper, right, lower)
+            minimum, maximum = img.crop(box).getextrema()
+
+            if upper > 1:
+                if bg_color == 'black':
+                    if vertical_result[-1][3] == 0 and maximum == 255:
+                        # Rectangle detects white color for the first time in a while -> Start of one line
+                        upper_bounds.append(upper)
+                    if vertical_result[-1][3] == 255 and maximum == 0:
+                        # Rectangle detects black color for the first time in a while -> End of one line
+                        lower_bounds.append(lower)
+                elif bg_color == 'white':
+                    if vertical_result[-1][2] == 255 and minimum == 0:
+                        # Rectangle detects black color for the first time in a while -> Start of one line
+                        upper_bounds.append(upper)
+                    if vertical_result[-1][2] == 0 and minimum == 255:
+                        # Rectangle detects white color for the first time in a while -> End of one line
+                        lower_bounds.append(lower)
+
+            # Storing all detection result
+            vertical_result.append([upper, lower, minimum, maximum])
+
+        final_result = []
+
+        line_count = 1
+
+        # Iterate through each line of code from detection
+        for upper_bound, lower_bound in list(zip(upper_bounds, lower_bounds)):
+
+            # Reset all temporary result for the next line
+            horizontal_result, left_bounds, right_bounds = [], [], []
+
+            # Move the detecting rectangle from the left to the right of the image
+            for left in range(width - margin_width):
+
+                right = left + margin_width
+
+                box = (left, upper_bound, right, lower_bound)
+                minimum, maximum = img.crop(box).getextrema()
+
+                if left > 1:
+                    if bg_color == 'black':
+                        if horizontal_result[-1][3] == 0 and maximum == 255:
+                            # Rectangle detects black color for the first time in a while -> Start of one word
+                            left_bounds.append(left)
+                        if horizontal_result[-1][3] == 255 and maximum == 0:
+                            # Rectangle detects white color for the first time in a while -> End of one word
+                            right_bounds.append(right)
+                    elif bg_color == 'white':
+                        if horizontal_result[-1][2] == 255 and minimum == 0:
+                            # Rectangle detects black color for the first time in a while -> Start of one word
+                            left_bounds.append(left)
+                        if horizontal_result[-1][2] == 0 and minimum == 255:
+                            # Rectangle detects white color for the first time in a while -> End of one word
+                            right_bounds.append(right)
+
+                # Storing all detection result
+                horizontal_result.append([left, right, minimum, maximum])
+
+            if level == 'sub-line':
+
+                part_count = 1
+
+                for left, right in list(zip(left_bounds, right_bounds)):
+                    final_result.append(
+                        ['sub-line', f'line {line_count} part {part_count}', left, upper_bound, right, lower_bound])
+                    part_count += 1
+
+            elif level == 'line':
+                final_result.append(
+                    ['line', f'line {line_count}', left_bounds[0], upper_bound, right_bounds[-1], lower_bound])
+
+            line_count += 1
+
+        # Format pandas dataframe
+        columns = ['kind', 'name', 'x', 'y', 'width', 'height', 'image']
+        aoi = pd.DataFrame(columns=columns)
+
+        for entry in final_result:
+            kind, name, x, y, x0, y0 = entry
+            width = x0 - x
+            height = y0 - y
+            image = image
+
+            # For better visualization
+            x += margin_width / 2
+            width -= margin_width
+
+            value = [kind, name, x, y, width, height, image]
+            dic = dict(zip(columns, value))
+
+            aoi = aoi.append(dic, ignore_index=True)
+
+        return aoi, bg_color
+
+
+######################## end from EMTK #################################
+
     '''find the areas of interest (aoi) for the selected stimulus'''
     def find_aoi(self):
         if self.file_path != '':
-            self.aoi, self.background_color = emtk.find_aoi(image=self.file_name, image_path=self.file_path.replace(self.file_name, ''))
+            self.aoi, self.background_color = self.EMTK_find_aoi(self.file_name, self.file_path.replace(self.file_name, ''))
 
     '''draw the found aois to the canvas'''
     def draw_aoi(self):
@@ -603,9 +800,9 @@ class Fix8(QMainWindow):
                 self.suggested_corrections[:, 0:2] = da.chain(copy.deepcopy(fixation_XY), line_Y)
                 self.relevant_buttons("algorithm_selected")
                 #self.update_suggestion()
-            elif self.algorithm == 'cluster':
-                self.suggested_corrections[:, 0:2] = da.cluster(copy.deepcopy(fixation_XY), line_Y)
-                self.relevant_buttons("algorithm_selected")
+            # elif self.algorithm == 'cluster':
+            #     self.suggested_corrections[:, 0:2] = da.cluster(copy.deepcopy(fixation_XY), line_Y)
+            #     self.relevant_buttons("algorithm_selected")
                 #self.update_suggestion()
             elif self.algorithm == 'merge':
                 self.suggested_corrections[:, 0:2] = da.merge(copy.deepcopy(fixation_XY), line_Y)
@@ -619,9 +816,9 @@ class Fix8(QMainWindow):
                 self.suggested_corrections[:, 0:2] = da.segment(copy.deepcopy(fixation_XY), line_Y)
                 self.relevant_buttons("algorithm_selected")
                 #self.update_suggestion()
-            elif self.algorithm == 'split':
-                self.suggested_corrections[:, 0:2] = da.split(copy.deepcopy(fixation_XY), line_Y)
-                self.relevant_buttons("algorithm_selected")
+            # elif self.algorithm == 'split':
+            #     self.suggested_corrections[:, 0:2] = da.split(copy.deepcopy(fixation_XY), line_Y)
+            #     self.relevant_buttons("algorithm_selected")
                 #self.update_suggestion()
             elif self.algorithm == 'stretch':
                 self.suggested_corrections[:, 0:2] = da.stretch(copy.deepcopy(fixation_XY), line_Y)
@@ -1039,11 +1236,11 @@ class Fix8(QMainWindow):
         self.dropdown_select_algorithm.addItem('Manual Correction')
         self.dropdown_select_algorithm.addItem('Attach')
         self.dropdown_select_algorithm.addItem('Chain')
-        self.dropdown_select_algorithm.addItem('Cluster')
+        # self.dropdown_select_algorithm.addItem('Cluster')
         self.dropdown_select_algorithm.addItem('Merge')
         self.dropdown_select_algorithm.addItem('Regress')
         self.dropdown_select_algorithm.addItem('Segment')
-        self.dropdown_select_algorithm.addItem('Split')
+        # self.dropdown_select_algorithm.addItem('Split')
         self.dropdown_select_algorithm.addItem('Stretch')
         # self.dropdown_select_algorithm.addItem('Compare')
         self.dropdown_select_algorithm.addItem('Warp')
