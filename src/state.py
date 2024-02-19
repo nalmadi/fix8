3a44f6be46c8df28e58f3b2a318538e08e328890
Naser Al Madi <nsalmadi@colby.edu>
2024-02-19 02:47:46 -0500

    fixed delete fixation in menu, better state history, and fixed undo

--- src/state.py
+++ src/state.py
@@ -3,8 +3,9 @@
 # this is needed for undo/redo feature
 # implemented using the Memento pattern
 
-class State:
+class History:
     def __init__(self):
+        # holds Fix8State objects
         self.states = []
 
     def is_empty(self):
@@ -14,5 +15,34 @@ class State:
         return self.states.pop()
     
     def set_state(self, state):
-        self.states.append(state.copy())
+        self.states.append(state)
         
+
+class Fix8State:
+    def __init__(self, fixations, saccades, blinks, suggested_corrections, current_fixation, selected_fixation):
+        if fixations is None:
+            self.fixations = []
+        else:
+            self.fixations = fixations.copy()
+
+        if saccades is None:
+            self.saccades = []
+        else:
+            self.saccades = saccades.copy()
+
+        if blinks is None:
+            self.blinks = []
+        else:
+            self.blinks = blinks.copy()
+
+        if suggested_corrections is None:
+            self.suggested_corrections = []
+        else:
+            self.suggested_corrections = suggested_corrections.copy()
+
+        self.current_fixation = current_fixation
+        self.selected_fixation = selected_fixation
+
+    def get_state(self):
+        return self.fixations, self.saccades, self.blinks, self.suggested_corrections, self.current_fixation, self.selected_fixation
+
