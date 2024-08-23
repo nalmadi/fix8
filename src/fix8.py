fb30cba9c13ed0741a7fa7313e9b8553731630c2
Naser Al Madi <nsalmadi@colby.edu>
2024-08-23 16:26:21 -0400

    saving metadata with CSV too for experiment 2

--- src/fix8.py
+++ src/fix8.py
@@ -2080,7 +2080,7 @@ class Fix8():
                 + "\n"
             )
 
-            #self.save_metadata_file(new_correction_file_name)
+            self.save_metadata_file(new_correction_file_name)
 
             self.status_text = "Corrections Saved to" + " " + new_correction_file_name
             self.ui.statusBar.showMessage(self.status_text)
